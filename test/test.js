beb74af1a8643872ff83ca8fadf9f23d7fa089d5
QuangSen <quangsen283@gmail.com>
2018-06-23 15:40:27 +0700

    Push test case

---
+++ test/test.js
@@ -0,0 +1,43 @@
+describe('Test case 1', function() {
+    it('Nguoi choi 1 bam vao mot o', function(done) {
+        var map = [];
+        assert.typeOf(foo, 'string');
+    });
+});
+
+describe('Test case 2', function() {
+    it('O nao da tich thi khong the duoc tich them nua', function(done) {
+        assert.typeOf(foo, 'string');
+    });
+});
+
+describe('Test case 3', function() {
+    it('Nguoi choi 1 tich thi phai den luot nguoi thu 2 ', function(done) {
+        assert.typeOf(foo, 'string');
+    });
+});
+
+describe('Test case 4', function() {
+    it('Nguoi choi tich duoc 5 o hang ngang thi bao thang va ket thuc', function(done) {
+        assert.typeOf(foo, 'string');
+    });
+});
+
+describe('Test case 5', function() {
+    it('Nguoi choi tich duoc 5 o hang cheo thi bao thang va ket thuc', function(done) {
+        assert.typeOf(foo, 'string');
+    });
+});
+
+describe('Test case 6', function() {
+    it('Nguoi choi tich duoc 5 o hang doc thi bao thang va ket thuc', function(done) {
+        assert.typeOf(foo, 'string');
+    });
+});
+
+describe('Test case 7', function() {
+    it('Bat dau game thi cac o chua duoc tich', function(done) {
+        assert.typeOf(foo, 'string');
+    });
+});
+
